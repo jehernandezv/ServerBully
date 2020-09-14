83723a3133d2fc9d1c9002c70df6abb2296d6b12
jehernandezv <jhonn.hernandez@uptc.edu.co>
2020-09-13 22:00:23 -0500

    update

--- js/client.js
+++ js/client.js
@@ -1,1 +1,8 @@
 var hostSocket = io('http://localhost:7000');
+
+document.getElementById('btn_give').addEventListener('click', async (event) => {
+    event.preventDefault();
+    console.log('give');
+    hostSocket.emit('give');
+    //aqui codigo para tabla en la vista de que ya no quiere ser lider
+});
--- server.js
+++ server.js
@@ -17,22 +17,32 @@ const server = app.listen(port, () => {
 
 var valueNode = Math.floor(Math.random() * (100 - 1) + 1);
 var listNodes = [];
-var give = 1;
 var leader = '';
 var isLeader = 0;
-var timeAlive = Math.floor(Math.random() * (5000 - 1000) + 1000);
+var interval;
+var give = 1;
+var recivedMax = 0;
+var timeAlive = timeRandom();
 var host = 'http://localhost:' + port;
 let hostSocket = io(server);
 
 hostSocket.on('connection', node => {
     console.log('Nueva conexión:', node.id);
+
+    //botton ya no quiero ser lider
+    node.on('give', () => {
+        console.log('Ya no quiero ser lider');
+        isLeader = 0;
+        give = 0;
+    });
+
     axios.post('http://localhost:4000/newConn', {
         value: valueNode,
         url: 'http://localhost:' + port
     }).then((response) => {
         console.log('new node: ' + response.data.list);
         listNodes = JSON.parse(response.data.list);
-
+        leader = '';
         listNodes.forEach(element => {
             if (element.url == host && element.leader == 1) {
                 //soy el lider?
@@ -58,6 +68,7 @@ app.post('/updateList', function (req, res, next) {
     console.log('update list: ' + data.list);
     listNodes = JSON.parse(data.list);
     //validar si soy el lider o quien es
+    leader = '';
     listNodes.forEach(element => {
         if (element.url == host && element.leader == 1) {
             //soy el lider?
@@ -65,6 +76,7 @@ app.post('/updateList', function (req, res, next) {
         } else if (element.leader == 1) {
             //quien es el lider?
             leader = element.url;
+            isLeader = 0;
             console.log('el lider es: ' + leader);
         }
 
@@ -75,6 +87,24 @@ app.post('/updateList', function (req, res, next) {
     res.json({
         message: 'Recibi update'
     });
+    give = 1;
+    interval = setInterval(latido, timeAlive);
+});
+
+app.post('/choise',function (req, res, next) {
+    console.log(req.body);
+    //parando las pulsaciones
+    clearInterval(interval);
+    //Evaluo si entro en la eleccion
+    if(req.body.value < valueNode){
+        console.log('entro en la elección');
+    res.json({
+        url:host,
+        value:valueNode
+    });
+  }else{
+    console.log('no responden por valor inferior');
+  }
 });
 
 app.get('/alive', function (req, res) {
@@ -83,18 +113,87 @@ app.get('/alive', function (req, res) {
     });
 });
 
+interval = setInterval(latido, timeAlive);
 
-function latido() {
-    console.log('value: ' + valueNode);
-    if(isLeader != 1){
-            axios.get(leader + '/alive')
+async function latido() {
+    console.log('timeRandom: ' + timeAlive);
+    if(isLeader == 0){
+            await axios.get(leader + '/alive')
                 .then((response) => {
                     console.log('¿QUIERE SEGUIR? ' + response.data.give);
-                    timeAlive = Math.floor(Math.random() * (5000 - 1000) + 1000);
+                    if(response.data.give == 1){
+                        timeAlive = timeRandom();
+                    }else{
+                        console.log('nueva eleccion, yo paro mi pulsación');
+                        clearInterval(interval);
+                        election();
+                    }
                 })
                 .catch((error) => {
-                   console.log(error);
+                   console.log(error.code);
+                   clearInterval(interval);
                 });
         }
 }
-setInterval(latido, timeAlive);
+
+function election(){
+    recivedMax = 0;
+    listNodes.forEach(element => {
+        if(element.url != host && element.url != leader){
+            axios.post(element.url+'/choise', {
+                value: valueNode,
+                url:host
+            })
+            .then((response) => {
+                console.log('llegan los nodos en elección');
+                console.log(response.data);
+                recivedMax = 1;
+                //metodo para elegir max
+                if(response.data.value == getMaxNode()){
+                    isLeader = 0;
+                    console.log('if de max node: ' + response.data.value);
+                    axios.post('http://localhost:4000/newLeader',{
+                             newLeader: response.data.url
+                        }).then((response) => {
+                            console.log(response.data.message);
+                        }).catch((error) => {
+                             console.log(error.code);
+                        });
+                    }
+            })
+            .catch((error) => {
+                console.log(error.code);
+                console.log('no responden por valor inferior');
+            })
+        }
+    });
+    if(recivedMax == 0 && valueNode == getMaxNode()){
+        console.log('detecto la caida y soy el lider');
+        axios.post('http://localhost:4000/newLeader',{
+            newLeader: host
+        }).then((response) => {
+            console.log(response.data.message);
+        }).catch((error) => {
+            console.log(error.code);
+        });
+
+        //aviso que soy el nuevo lider
+        /*listNodes.forEach(element => {
+            axios.post(element.url)
+        });*/
+    }
+}
+
+function getMaxNode(){
+    var max = 0;
+    listNodes.forEach(element => {
+        if(max < element.value && element.url != leader){
+            max = element.value;
+        }
+    });
+    return max;
+}
+
+function timeRandom(){
+    return Math.floor(Math.random() * (10000 - 1000) + 1000);
+}
